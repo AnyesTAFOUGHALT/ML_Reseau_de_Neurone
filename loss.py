f8a26f6ff295229fabda08adc520c0e00ccdc026
AnyesTAFOUGHALT <anyestafoughalt@gmail.com>
2024-04-12 13:36:51 +0200

    modele lineare v1

--- loss.py
+++ loss.py
@@ -7,3 +7,16 @@ class Loss(object):
 
     def backward(self, y, yhat):
         pass
+
+
+class MSELoss(Loss):
+    def forward(self, y, yhat):
+        # y et yhat sont de taille batch * d
+        assert y.shape == yhat.shape
+
+        #return np.sum((y - yhat)**2, axis=1)  / y.shape[0]
+        return np.sum((y - yhat)**2, axis=1) 
+
+    def backward(self, y, yhat):
+        assert y.shape == yhat.shape
+        return -2 * (y - yhat) 
--- modele.py
+++ modele.py
@@ -24,3 +24,38 @@ class Module(object):
     def backward_delta(self, input, delta):
         ## Calcul la derivee de l'erreur
         pass
+
+
+class Module_lineare(Module):
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self._parameters = {
+            'weights': np.random.randn(input_size, output_size),
+            'bias': np.zeros(output_size)
+        }
+
+    def zero_grad(self):
+        ## Annule gradient
+        self._gradient = None
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        # X : (n , d_input) , W : (d_input , d_output)
+        return X @ self._parameters['weights'] + self._parameters['bias']
+    
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        self.gradient['weights'] += np.dot(input.T, delta)
+        self.gradient['bias'] += np.sum(delta, axis=0)
+
+    def backward_delta(self, input, delta):
+        # Calcul du gradient par rapport aux entrées
+        return np.dot(delta, self._parameters['weights'].T)
+
+    def update_parameters(self, gradient_step):
+        # Mise à jour des paramètres avec un certain pas d'apprentissage
+        self._parameters['weights'] -= gradient_step * self.gradient['weights']
+        self._parameters['bias'] -= gradient_step * self.gradient['bias']
+
