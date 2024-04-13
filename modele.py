6c430d0ddcbfe0edee29c822798e14e6e96b02a8
AnyesTAFOUGHALT <anyestafoughalt@gmail.com>
2024-04-13 12:28:49 +0200

    module lineaire v2

--- modele.py
+++ modele.py
@@ -27,35 +27,50 @@ class Module(object):
 
 
 class Module_lineare(Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size , biais = False):
         super().__init__()
         self.input_size = input_size
         self.output_size = output_size
+        self.biais = biais
         self._parameters = {
             'weights': np.random.randn(input_size, output_size),
-            'bias': np.zeros(output_size)
+            'biais': np.zeros(output_size)
         }
+
+        self.zero_grad()
 
     def zero_grad(self):
         ## Annule gradient
-        self._gradient = None
+        self._gradient['weights'] = np.zeros((self.input_size, self.output_size))
+        self._gradient['biais'] = np.zeros(self.output_size)
 
     def forward(self, X):
         ## Calcule la passe forward
         # X : (n , d_input) , W : (d_input , d_output)
-        return X @ self._parameters['weights'] + self._parameters['bias']
+        assert X.shape[1] == self.input_size
+        if self.biais :
+            return X @ self._parameters['weights'] + self._parameters['biais']
+        return X @ self._parameters['weights'] 
     
     def backward_update_gradient(self, input, delta):
         ## Met a jour la valeur du gradient
-        self.gradient['weights'] += np.dot(input.T, delta)
-        self.gradient['bias'] += np.sum(delta, axis=0)
+        assert input.shape[1] == self.input_size
+        assert delta.shape[1] == self.output_size
+        assert input.shape[0] == delta.shape[0]
+        self.gradient['weights'] += input.T @ delta / len(input)
+        if self.biais :
+            self.gradient['biais'] += np.mean(delta, axis=0)
 
     def backward_delta(self, input, delta):
         # Calcul du gradient par rapport aux entrées
-        return np.dot(delta, self._parameters['weights'].T)
+        assert input.shape[1] == self.input_size
+        assert delta.shape[1] == self.output_size
+        assert input.shape[0] == delta.shape[0]
+        return delta @ self._parameters['weights'].T
 
     def update_parameters(self, gradient_step):
         # Mise à jour des paramètres avec un certain pas d'apprentissage
         self._parameters['weights'] -= gradient_step * self.gradient['weights']
-        self._parameters['bias'] -= gradient_step * self.gradient['bias']
+        if self.biais :
+            self._parameters['biais'] -= gradient_step * self.gradient['biais']
 
