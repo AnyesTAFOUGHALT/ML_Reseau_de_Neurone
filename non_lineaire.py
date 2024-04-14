4e4ed069c00ee14e897a52ca26504bd4555c9c94
racha1004 <rachanadine2002@gmail.com>
2024-04-14 19:29:30 +0200

    TanH

--- non_lineaire.py
+++ non_lineaire.py
@@ -1,33 +1,29 @@
 import numpy as np
 import modele
+import utils as ut
 
 class TanH(modele.Module):
 
     def __init__(self):
+        super().__init__()
         self._parameters = None
         self._gradient = None
 
-    def zero_grad(self):
-        ## Annule gradient
-        pass
 
     def forward(self, X):
         ## Calcule la passe forward
+        assert X.shape[1] == self.input_size
         return np.tanh(X) 
 
-    def update_parameters(self, gradient_step=1e-3):
-        ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
-        self._parameters -= gradient_step*self._gradient
-
-    def backward_update_gradient(self, input, delta):
-        ## Met a jour la valeur du gradient
-        pass
 
     def backward_delta(self, input, delta):
         ## Calcul la derivee de l'erreur
-        pass
-
-
+        # Calcul du gradient par rapport aux entrées
+        assert input.shape[1] == self.input_size # chaque exemple x doit avoir une dimension egale au nombre d'entrees supportées par le reseau de neurones 
+        assert delta.shape[1] == self.output_size # pour chaque delta on a une valeur pour calcule pour chaque noeurone de sortie
+        assert input.shape[0] == delta.shape[0] # autant de d'exmeples que de delta genere
+        return (1-np.tanh(input)**2) * delta 
+    
 
 class Sigmoide(modele.Module):
 
@@ -35,22 +31,16 @@ class Sigmoide(modele.Module):
         self._parameters = None
         self._gradient = None
 
-    def zero_grad(self):
-        ## Annule gradient
-        pass
 
     def forward(self, X):
         ## Calcule la passe forward
+        assert X.shape[1] == self.input_size
         return 1/(1+np.exp(-X))
-
-    def update_parameters(self, gradient_step=1e-3):
-        ## Calcule la mise a jour des parametres selon le gradient calcule et le pas de gradient_step
-        self._parameters -= gradient_step*self._gradient
-
-    def backward_update_gradient(self, input, delta):
-        ## Met a jour la valeur du gradient
-        pass
 
     def backward_delta(self, input, delta):
         ## Calcul la derivee de l'erreur
-        pass
+        # Calcul du gradient par rapport aux entrées
+        assert input.shape[1] == self.input_size # chaque exemple x doit avoir une dimension egale au nombre d'entrees supportées par le reseau de neurones 
+        assert delta.shape[1] == self.output_size # pour chaque delta on a une valeur pour calcule pour chaque noeurone de sortie
+        assert input.shape[0] == delta.shape[0] # autant de d'exmeples que de delta genere
+        return delta * ut.sigmoid_derivative(input).T
