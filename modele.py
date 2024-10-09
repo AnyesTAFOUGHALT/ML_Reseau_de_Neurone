87a0bdb16f5cd3aea4bed4fa0e611144342b9dd3
AnyeTAFOUGHALT <tafoughaltanyes@gmail.com>
2024-10-09 06:57:25 +0200

    final verion

--- modele.py
+++ modele.py
@@ -1,9 +1,11 @@
 import numpy as np
+import utils as  ut 
 
 class Module(object):
     def __init__(self):
         self._parameters = None
         self._gradient = None
+        self._input = None
 
     def zero_grad(self):
         ## Annule gradient
@@ -24,3 +26,336 @@ class Module(object):
     def backward_delta(self, input, delta):
         ## Calcul la derivee de l'erreur
         pass
+
+
+#------------------- Linéaire -------------------#
+
+class Module_lineare(Module):
+
+    def __init__(self, input_size, output_size , biais = True):
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.biais = biais
+        self._parameters = {
+            'weights':np.random.random((input_size, output_size))-0.5,
+            'biais': np.random.randn(output_size)-0.5,
+        }
+        self._gradient = dict()
+        self.zero_grad()
+
+
+
+    def zero_grad(self):
+        ## Annule gradient
+        self._gradient['weights'] = np.zeros((self.input_size, self.output_size))
+        self._gradient['biais'] = np.zeros(self.output_size)
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        # X : (n , d_input) , W : (d_input , d_output)
+        assert X.shape[1] == self.input_size
+        self._input = X.copy()
+        if self.biais :
+            return X @ self._parameters['weights'] + self._parameters['biais']
+        return X @ self._parameters['weights'] 
+    
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        assert input.shape[1] == self.input_size
+        assert delta.shape[1] == self.output_size
+        assert input.shape[0] == delta.shape[0]
+        self._gradient['weights'] += input.T @ delta
+        if self.biais :
+            self._gradient['biais'] += np.sum(delta, axis=0)
+
+    def backward_delta(self, input, delta):
+        # Calcul du gradient par rapport aux entrées
+        assert input.shape[1] == self.input_size
+        assert delta.shape[1] == self.output_size
+        assert input.shape[0] == delta.shape[0]
+        return delta @ self._parameters['weights'].T
+
+    def update_parameters(self, gradient_step):
+        # Mise à jour des paramètres avec un certain pas d'apprentissage
+        self._parameters['weights'] -= gradient_step * self._gradient['weights']
+        if self.biais :
+            self._parameters['biais'] -= gradient_step * self._gradient['biais']
+
+    def set_parameters(self, parameters):
+        assert parameters['weights'].shape == self._parameters['weights'].shape
+        assert parameters['biais'].shape == self._parameters['biais'].shape
+        self._parameters['weights'] = parameters['weights'].copy()
+        self._parameters['biais'] = parameters['biais'].copy()
+
+    
+    def get_parameters(self):
+        parameters = dict()
+        parameters['weights'] = self._parameters['weights'].copy()
+        parameters['biais'] = self._parameters['biais'].copy()
+        return parameters
+    
+
+#------------------- Non_Linéaire -------------------#
+
+class TanH(Module):
+
+    def __init__(self):
+        super().__init__()
+        self._parameters = None
+        self._gradient = None
+
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        self._input = X
+        return np.tanh(X) 
+
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        # Calcul du gradient par rapport aux entrées
+        return (1-np.tanh(input)**2) * delta 
+    
+
+class Sigmoide(Module):
+
+    def __init__(self):
+        self._parameters = None
+        self._gradient = None
+
+
+    def forward(self, X):
+        ## Calcule la passe forward
+        self._input = X
+        return 1/(1+np.exp(-X))
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        # Calcul du gradient par rapport aux entrées
+        return delta * ut.sigmoid_derivative(input)
+
+
+class Softmax(Module):
+    def __init__(self ):
+        super().__init__()
+
+    def forward(self, x):
+        ## Calcule la passe forward
+        self._input = x
+        eps = 1e-4
+        exps = np.exp(x) 
+        return exps / (np.sum(exps, axis=1, keepdims=True) + eps)
+
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        eps = 1e-4
+        exps = np.exp(input)
+        q = exps / (np.sum(exps, axis=1, keepdims=True) + eps )
+        return delta * q * (1 - q)
+    
+#-------------------------- ReLu -----------------------#
+    
+class ReLu(Module):
+    def __init__(self, threshold=0 ):
+        super().__init__()
+        self._threshold = threshold
+
+    def forward(self, x):
+        ## Calcule la passe forward
+        self._input = x
+        return np.where(x>self._threshold,x,0.)
+    
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        return np.where(input>self._threshold,1.,0.) * delta 
+
+    def update_parameters(self, gradient_step=1e-3):     
+        pass
+
+    def backward_update_gradient(self, input, delta):
+        pass
+
+#-------------------------- Conv1D -----------------------#   
+class Conv1D(Module):
+    def __init__(self,k_size,chan_in,chan_out,stride):
+        """
+        k_size : kernel size
+        chan_in : number of input channels
+        chan_out : number of output channels
+        stride : stride value
+        """
+        self.k_size = k_size
+        self.chan_in = chan_in
+        self.chan_out = chan_out
+        self.stride = stride
+
+        np.random.seed(0)
+        self._parameters =2*(np.random.random((k_size,chan_in,chan_out))-0.5) * 1e-1 
+        self._gradient = np.zeros((k_size,chan_in,chan_out))
+
+    def forward(self,X):
+        """
+            X est de taille  (batch,length,chan_in)
+            output est de taille (batch, (length-k_size)/stride +1,chan_out)
+        """
+        self._input = X.copy()
+        batch_size,input_length,c = X.shape
+
+        result = np.zeros((batch_size, (input_length-self.k_size)//self.stride + 1, self.chan_out))
+        for n in range(batch_size):
+            A = X[n]
+            for f in range(self.chan_out):
+                weights = self._parameters[:,:,f] 
+
+                for i in range(0,result.shape[1]):
+                    j = i*self.stride
+                    B = A[j:j+self.k_size] 
+                    
+                    result[n,i,f] = (B * weights).sum()
+
+        return result
+
+    def backward_update_gradient(self, input, delta):
+        """
+        input de taille (batch, length, chan_in)
+        delta de taille (batch, output_length, chan_out)
+        """
+        assert input.shape[2] == self.chan_in
+        assert delta.shape[2] == self.chan_out
+        assert delta.shape[1] == (input.shape[1]-self.k_size)//self.stride + 1
+        assert delta.shape[0] == input.shape[0]
+        batch_size, length_out, chan_out = delta.shape
+        for n in range(batch_size):
+            A = input[n] 
+            for z in range(chan_out):
+                for i in range(length_out):
+                    Xs = A[i:i+self.k_size] 
+                    delta0 = delta[n,i,z]
+                    self._gradient[:,:,z] += Xs*delta0
+    def update_parameters(self, gradient_step=1e-5):
+        pass
+
+    def backward_delta(self, input, delta):
+        """
+        input de taille (batch, length, chan_in)
+        delta de taille (batch, output_length, chan_out)
+        Returns:
+        - delta de taille (batch, length, chan_in)
+        """   
+
+        b, length_out, chan_out = delta.shape
+        
+        result = np.zeros(input.shape)
+        result = np.array(result, dtype=np.float64)
+
+        for n in range(b):
+            A = input[n] 
+            for z in range(chan_out):
+                weights = self._parameters[:,:,z] 
+                for i in range(length_out):
+                    delta0 = delta[n,i,z]
+                    result[n,i:i+self.k_size,:] += weights*delta0
+        return result
+    
+    def zero_grad(self):
+        self._gradient = np.zeros(self._gradient.shape)
+
+#-------------------------- MaxPool1D -----------------------#  
+class MaxPool1D(Module):
+    def __init__(self,k_size,stride):
+        """
+        k_size : kernel size
+        stride : stride value
+        """
+        self.k_size = k_size
+        self.stride = stride
+        self.max_idx = None
+    
+    def forward(self,X):
+        """
+        data: input data of shape (batch, length, chan_in)
+        Returns:
+        - output of shape (batch, (length - k_size) // stride + 1, chan_in)
+        """
+        self._input = X.copy()
+        batch_size,input_length,chan_in = X.shape
+
+        result = np.zeros((batch_size, (input_length-self.k_size)//self.stride + 1, chan_in))
+        self.max_idx = np.zeros(result.shape)
+
+        for i in range(0, result.shape[1]):
+            
+            self.max_idx[:,i] = np.argmax(X[:, (i * self.stride): (i * self.stride + self.k_size)],
+                                                 axis=1) + i * self.stride
+            result[:,i,:] = np.max(X[:,(i*self.stride) : (i*self.stride + self.k_size)], axis=1)
+        self.max_idx = self.max_idx.astype(int)
+        return result
+
+    def update_parameters(self, gradient_step=1e-5):      
+        pass
+        
+
+    def backward_update_gradient(self, input, delta):      
+        pass
+
+    def backward_delta(self, input, delta):
+        """
+        input: input data of shape (batch, length, chan_in)
+        delta: delta from the next layer of shape (batch, output_length, chan_in)
+        Returns:
+        - delta of shape (batch, length, chan_in)
+        """
+        batch_size,input_length,chan_in = input.shape        
+        result = np.zeros_like(input)
+        
+        for n in range(batch_size):
+            A = input[n]
+            ind = self.max_idx[n]
+            for i in range(ind.shape[0]):
+                for j in range(ind.shape[1]):
+                    result[n,ind[i,j],j] = delta[n,i,j]
+        
+        return result
+
+#-------------------------- Flatten -----------------------#  
+    
+class Flatten(Module):
+    def __init__(self):
+        pass
+    
+    def forward(self,X):
+        '''
+        params:
+        -------
+        X : dim (batch,length,chan_in)
+
+        return:
+        -------
+        dim (batch,length*chan_in)
+        '''
+        self._input = X.copy()
+        return X.reshape((len(X),-1))
+        
+    def backward_delta(self, input, delta):
+        ## Calcul la derivee de l'erreur
+        '''
+        param:
+        ------
+        input : resPool, dim (batch,length,chan_in)
+        delta : delta of lin, dim (batch,length*chan_out)
+        
+        return:
+        dim (batch,length,chan_in)
+        '''
+        return delta.reshape(input.shape)
+
+    def backward_update_gradient(self, input, delta):
+        ## Met a jour la valeur du gradient
+        pass
+
+    def update_parameters(self, gradient_step=1e-5):
+        '''
+        no parameter
+        '''        
+        pass    
---
+++ utils.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score, completeness_score, homogeneity_score, v_measure_score
+
+
+def plot_data(data,labels=None):
+    """
+    Affiche des donnees 2D
+    :param data: matrice des donnees 2d
+    :param labels: vecteur des labels (discrets)
+    :return:
+    """
+    if labels is not None:
+        labels = labels.reshape(-1)
+    cols,marks = ["red", "green", "blue", "orange", "black", "cyan"],[".","+","*","o","x","^"]
+    if labels is None:
+        plt.scatter(data[:,0],data[:,1],marker="x")
+        return
+    for i,l in enumerate(sorted(list(set(labels.flatten())))):
+        plt.scatter(data[labels==l,0],data[labels==l,1],c=cols[i],marker=marks[i])
+
+def plot_frontiere(data,f,step=20):
+    """ Trace un graphe de la frontiere de decision de f
+    :param data: donnees
+    :param f: fonction de decision
+    :param step: pas de la grille
+    :return:
+    """
+    grid,x,y=make_grid(data=data,step=step)
+    plt.contourf(x,y,f(grid).reshape(x.shape),colors=('gray','blue'),levels=[-1,0,1])
+
+def make_grid(data=None,xmin=-5,xmax=5,ymin=-5,ymax=5,step=20):
+    """ Cree une grille sous forme de matrice 2d de la liste des points
+    :param data: pour calcluler les bornes du graphe
+    :param xmin: si pas data, alors bornes du graphe
+    :param xmax:
+    :param ymin:
+    :param ymax:
+    :param step: pas de la grille
+    :return: une matrice 2d contenant les points de la grille
+    """
+    if data is not None:
+        xmax, xmin, ymax, ymin = np.max(data[:,0]),  np.min(data[:,0]), np.max(data[:,1]), np.min(data[:,1])
+    x, y =np.meshgrid(np.arange(xmin,xmax,(xmax-xmin)*1./step), np.arange(ymin,ymax,(ymax-ymin)*1./step))
+    grid=np.c_[x.ravel(),y.ravel()]
+    return grid, x, y
+
+def gen_arti(centerx=1,centery=1,sigma=0.1,nbex=1000,data_type=0,epsilon=0.02):
+    """ Generateur de donnees,
+        :param centerx: centre des gaussiennes
+        :param centery:
+        :param sigma: des gaussiennes
+        :param nbex: nombre d'exemples
+        :param data_type: 0: melange 2 gaussiennes, 1: melange 4 gaussiennes, 2:echequier
+        :param epsilon: bruit dans les donnees
+        :return: data matrice 2d des donnnes,y etiquette des donnnees
+    """
+    if data_type==0:
+         #melange de 2 gaussiennes
+         xpos=np.random.multivariate_normal([centerx,centerx],np.diag([sigma,sigma]),nbex//2)
+         xneg=np.random.multivariate_normal([-centerx,-centerx],np.diag([sigma,sigma]),nbex//2)
+         data=np.vstack((xpos,xneg))
+         y=np.hstack((np.ones(nbex//2),-np.ones(nbex//2)))
+    if data_type==1:
+        #melange de 4 gaussiennes
+        xpos=np.vstack((np.random.multivariate_normal([centerx,centerx],np.diag([sigma,sigma]),nbex//4),np.random.multivariate_normal([-centerx,-centerx],np.diag([sigma,sigma]),nbex//4)))
+        xneg=np.vstack((np.random.multivariate_normal([-centerx,centerx],np.diag([sigma,sigma]),nbex//4),np.random.multivariate_normal([centerx,-centerx],np.diag([sigma,sigma]),nbex//4)))
+        data=np.vstack((xpos,xneg))
+        y=np.hstack((np.ones(nbex//2),-np.ones(nbex//2)))
+
+    if data_type==2:
+        #echiquier
+        data=np.reshape(np.random.uniform(-4,4,2*nbex),(nbex,2))
+        y=np.ceil(data[:,0])+np.ceil(data[:,1])
+        y=2*(y % 2)-1
+    # un peu de bruit
+    data[:,0]+=np.random.normal(0,epsilon,nbex)
+    data[:,1]+=np.random.normal(0,epsilon,nbex)
+    # on mélange les données
+    idx = np.random.permutation((range(y.size)))
+    data=data[idx,:]
+    y=y[idx]
+    return data,y.reshape(-1, 1)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    sig = sigmoid(x)
+    return sig * (1 - sig)
+
+
+
+#--------------Chargement des données--------------------#
+def load_usps(fn):
+    with open(fn,"r") as f:
+        f.readline()
+        data = [[float(x) for x in l.split()] for l in f if len(l.split())>2]
+    tmp=np.array(data)
+    return tmp[:,1:],tmp[:,0].astype(int)
+
+def get_usps(l,datax,datay):
+    if type(l)!=list:
+        resx = datax[datay==l,:]
+        resy = datay[datay==l]
+        return resx,resy
+    tmp =   list(zip(*[get_usps(i,datax,datay) for i in l]))
+    tmpx,tmpy = np.vstack(tmp[0]),np.hstack(tmp[1])
+    return tmpx,tmpy
+
+def show_usps(data):
+    plt.imshow(data.reshape((16,16)),interpolation="nearest",cmap="gray")
+
+def show_usps_mnist(data):
+    plt.imshow(data.reshape((28,28)),interpolation="nearest",cmap="gray")
+
+
+#----------------Calcul performance---------------------#
+def prefomance_en_clustering(y_pred,y_true):
+    # Calcul des différentes métriques
+    ari = adjusted_rand_score(y_true, y_pred)
+    ami = adjusted_mutual_info_score(y_true, y_pred)
+    completeness = completeness_score(y_true, y_pred)
+    homogeneity = homogeneity_score(y_true, y_pred)
+    v_measure = v_measure_score(y_true, y_pred)
+
+    # Affichage des résultats
+    print("Adjusted Rand Index:", ari)
+    print("Adjusted Mutual Information:", ami)
+    print("Completeness Score:", completeness)
+    print("Homogeneity Score:", homogeneity)
+    print("V-measure Score:", v_measure)
+
+
+def one_hot(labels) :
+    onehot = np.zeros((labels.size, 10))
+    for i, label in enumerate(labels):
+        onehot[i, label] = 1
+    return onehot
+
+
+def add_noise(image, noise_factor=0.2):
+    return image + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=image.shape)
+    
